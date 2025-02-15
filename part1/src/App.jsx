42a25de30be4f19ac2a433714782216bcabd19b1
Othman Tabeche <otabeche23@gmail.com>
2025-02-15 20:15:56 +0100

    courseinfo 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,4 @@
+/*
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
@@ -27,19 +28,30 @@ const Total = (props) => {
     </div>
   )
 }
+*/
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {name: 'Fundamentals of React', exercice: 10},
-    {name: 'Using props to pass data', exercice: 7},
-    {name: 'State of a component', exercice: 14}
-  ]
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts}/>
+      <h1> {course} </h1>
+      <p> {part1.name} {part1.exercises} </p>
+      <p> {part2.name} {part2.exercises} </p>
+      <p> {part3.name} {part3.exercises} </p>
+      <h1>Hola</h1>
     </div>
   )
 }
