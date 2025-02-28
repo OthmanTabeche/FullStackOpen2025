08984487c54029728f800a62f065877b0649fa8e
Othman Tabeche <otabeche23@gmail.com>
2025-02-28 12:36:56 +0100

    unicafe 1.10

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+const StatisticLine = ( {text, value} ) => {
+ return <p> {text}: {value} </p>
+}
+
 const Statistics = ( {good, neutral, bad, all, average, positive} ) => {
     
   if (all === 0) {
@@ -7,15 +11,14 @@ const Statistics = ( {good, neutral, bad, all, average, positive} ) => {
   }
   
   return (
-      <div>
-        <h1>statics</h1>
-        <p>good: {good} </p>
-        <p>neutral: {neutral} </p>
-        <p>bad: {bad} </p>
-        <p>all: {all}</p>
-        <p>average: {average}</p>
-        <p>positive: {positive}</p>
-      </div>
+    <div>
+      <h1>statics</h1>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
+    </div>
     )
 }
 
