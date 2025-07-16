e6ea84f5421283b14633d3fcf9aad6dba920dcd0
othmantabeche <othmantabeche@gmail.com>
2025-07-16 21:11:46 +0200

    exercice 3.16 part3

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -15,7 +15,6 @@ app.use(express.static('dist'))
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGODB_URI)
 
-
 app.get('/api/persons', (request, response) => {
   Person.find({})
     .then(persons => {
@@ -41,24 +40,29 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-})
-
-app.delete('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id)
-    .then(() => {
-      response.status(202).end()
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
     .catch(error => {
       console.log(error)
       response.status(500).end()
     })
-  })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(202).end()
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
 })
 
 app.post('/api/persons', (request, response) => {
